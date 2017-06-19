b6f068708daee88966fd6aadf97b762a7401718f
Derek Lo <dlo102789@gmail.com>
2017-06-19 17:23:51 -0500

    Added pages migration file and contacts mailer functions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,47 @@
 class PagesController < ApplicationController
 	def home
+		@pages = Page.all
 	end
 
 	def about
+	end
+
+	def new
+		@page = Page.new
+	end
+
+	def create
+		@page = Page.new(page_params)
+		if @page.save
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			body = params[:contact][:comments]
+			ContactMailer.contact_email(name, email, body).deliver
+			flash[:success] = "Post Created!"
+			redirect_to page_path(@page)
+		else
+			flash[:danger] = "Error Occurred"
+			redirect_to page_path(@page)
+		end
+	end
+
+	def show
+		@page = Page.find(params[:id])
+	end
+
+	def destroy
+		@page = Page.find(params[:id])
+		@page.destroy
+		flash[:success] = "Article '#{@page.title}' deleted!"
+		redirect_to action: "home"
+	end
+
+	def edit
+		@page = Page.find(params[:id])
+	end
+
+	private
+	def page_params
+		params.require(:page).permit(:title, :body)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616134411) do
+ActiveRecord::Schema.define(version: 20170619204454) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
