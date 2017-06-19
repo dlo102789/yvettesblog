e1a6d64706515f0ae799431a1fb399a8e80cd676
Derek Lo <dlo102789@gmail.com>
2017-06-19 14:59:31 -0500

    Fixed routes, added validations in contact model file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(contact_params)
 		if @contact.save
-			redirect_to new_contact_path, notice: "Message sent."
+			flash[:success] = "Message Sent"
+			redirect_to new_contact_path
 		else
-			redirect_to new_contact_path, notice: "Error occurred."
+			flash[:danger] = "Error Occurred"
+			redirect_to new_contact_path
 		end
 	end
 
