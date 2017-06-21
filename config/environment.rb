0b423b656218cd499c913280c1034f77114f264b
Derek Lo <dlo102789@gmail.com>
2017-06-21 16:17:02 -0500

    Switched to SendGrid. Config vars found with Heroku config command

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => 587,
+  :address        => 'smtp.sendgrid.net',
+  :user_name      => app71074003@heroku.com,
+  :password       => lbqfuvr63783,
+  :domain         => 'derekandyvette.herokuapp.com',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+  ActionMailer::Base.delivery_method = :smtp
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,5 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.digest = true
 
-  ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'derekandyvette.herokuapp.com',
-  :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
 
 end
