c7f38e4b7d7040e6d31413119e3e9f951826a17f
Derek Lo <dlo102789@gmail.com>
2017-06-21 16:28:09 -0500

    Debugged environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings {
+ActionMailer::Base.smtp_settings = {
   :port           => 587,
   :address        => 'smtp.sendgrid.net',
-  :user_name      => app71074003@heroku.com,
-  :password       => lbqfuvr63783,
+  :user_name      => 'app71074003@heroku.com',
+  :password       => 'lbqfuvr63783',
   :domain         => 'derekandyvette.herokuapp.com',
   :authentication => :plain,
   :enable_starttls_auto => true
